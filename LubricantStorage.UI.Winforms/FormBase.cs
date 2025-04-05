f048ede37f4fb37cdac49d2a331d236ded10d69f
Валерий Сошин <105991605+Valery-Soshin@users.noreply.github.com>
2025-04-05 22:18:10 +0300

    update

--- LubricantStorage.UI.Winforms/FormBase.cs
+++ LubricantStorage.UI.Winforms/FormBase.cs
@@ -8,7 +8,7 @@
         {
             _httpClient = new HttpClient
             {
-                BaseAddress = new Uri("http://localhost:5173"),
+                BaseAddress = new Uri("https://lubstorage.ru/"),
             };
 
             InitializeForm();
