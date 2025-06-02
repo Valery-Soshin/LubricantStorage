2a90309abd6346ac0145edbc0ed933f4c649a225
Валерий Сошин <105991605+Valery-Soshin@users.noreply.github.com>
2025-06-02 23:29:18 +0300

    Добавление TelegramBot

---
+++ LubricantStorage.API/Controllers/V1/TelegramBotController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace LubricantStorage.API.Controllers.V1
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/webhook")]
+    public class TelegramBotController : ControllerBase
+    {
+        private readonly ITelegramBotClient _botClient;
+
+        public TelegramBotController(ITelegramBotClient botClient)
+        {
+            _botClient = botClient;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Update update)
+        {
+            var message = update.Message;
+
+            if (message != null && message.Text != null)
+            {
+                await _botClient.SendMessage(
+                    chatId: message.Chat.Id,
+                    text: $"Вы сказали: {message.Text}");
+            }
+
+            return Ok();
+        }
+    }
+}
